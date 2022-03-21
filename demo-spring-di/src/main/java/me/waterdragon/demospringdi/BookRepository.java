7e51a42473fdc87a96d4bf6075fc905c4236f70e
waterdragon <tndyd5390@naver.com>
2022-03-21 23:01:37 +0900

    di

---
+++ demo-spring-di/src/main/java/me/waterdragon/demospringdi/BookRepository.java
@@ -0,0 +1,7 @@
+package me.waterdragon.demospringdi;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+}
---
+++ demo-spring-di/src/test/java/me/waterdragon/demospringdi/DemoSpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.waterdragon.demospringdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
